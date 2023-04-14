b74cd9eb1b82317c1154030d99354ad250f927b3
Nikhil Pandit <nikhil.pandit@cldcvr.com>
2023-04-14 15:48:57 +0530

    ns

--- challenge-1/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket = "<bucket>"
-    prefix = "terraform-state/app2/"
-  }
-}
--- challenge-2/metadata.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-instance=$1
-proj=$2
-zone=$3
-
-gcloud compute instances describe ${instance} --zone  ${zone}    --project ${proj} --format=json > ${instance}.json
- 
-# ./metadata.sh vpc-0 project1 asia-south1-c
